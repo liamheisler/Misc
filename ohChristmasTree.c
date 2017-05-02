9e92ad792c86a1c1614aa4911b69af4b89f8e80f
liamheisler <liammheisler@gmail.com>
2017-05-02 16:33:01 -0400

    Update ohChristmasTree.c

--- ohChristmasTree.c
+++ ohChristmasTree.c
@@ -27,130 +27,128 @@ int PIN = 4; //This determines which pin the speaker is wired to.
 #define G 10
 #define Gsharp 11
 
-void Octave(float Abase, int *notes);      //   declares the function Octave.
+void Octave(float Abase, int *notes); // declares the function Octave.
 
 int main()
 {
-  int notes[12];                              //establishes an array called “notes”  with 12 elements
+int notes[12]; //establishes an array called “notes” with 12 elements
 
-  Octave(BaseNote,notes);          //calls the octave function and sets the base frequency
+Octave(BaseNote,notes); //calls the octave function and sets the base frequency
 
-   //These print statements verify note values that the   Propeller is using
-  print("the freq of notes[B] is %d\n",notes[B]);
-  print("the freq of notes[A] is %d\n",notes[A]);
-  print("the freq of notes[E] is %d\n",notes[E]);
-  print("the freq of A2 is %d\n",2*(int)notes[A]);
+//These print statements verify note values that the Propeller is using
+print("the freq of notes[B] is %d\n",notes[B]);
+print("the freq of notes[A] is %d\n",notes[A]);
+print("the freq of notes[E] is %d\n",notes[E]);
+print("the freq of A2 is %d\n",2*(int)notes[A]);
 
-  while(1)
-  {
-    freqout(PIN,qnt,notes[C]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt,notes[G]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,3*qnt/2,2*notes[A]);
-    freqout(PIN,qnt/4,2*notes[A]);
+while(1)
+{
+freqout(PIN,qnt,notes[C]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt,notes[G]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,3*qnt/2,2*notes[A]);
+freqout(PIN,qnt/4,2*notes[A]);
 
-    freqout(PIN,qnt/2,notes[G]);
-    //freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,qnt,2*notes[Asharp]);
-    freqout(PIN,qnt,notes[E]);
-    freqout(PIN,qnt/2,notes[G]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt/2,notes[F]);
-    //freqout(PIN,qnt/2,notes[Csharp]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt,notes[G]);
+freqout(PIN,qnt/2,notes[G]);
+//freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,qnt,2*notes[Asharp]);
+freqout(PIN,qnt,notes[E]);
+freqout(PIN,qnt/2,notes[G]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt/2,notes[F]);
+//freqout(PIN,qnt/2,notes[Csharp]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt,notes[G]);
 
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,3*qnt/2,2*notes[A]);
-    freqout(PIN,qnt/4,2*notes[A]);
-    freqout(PIN,qnt,notes[G]);
-    //freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,qnt,2*notes[Asharp]);
-    freqout(PIN,qnt,notes[E]);
-    freqout(PIN,qnt/2,notes[G]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt,2*notes[C]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,3*qnt/2,2*notes[A]);
+freqout(PIN,qnt/4,2*notes[A]);
+freqout(PIN,qnt,notes[G]);
+//freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,qnt,2*notes[Asharp]);
+freqout(PIN,qnt,notes[E]);
+freqout(PIN,qnt/2,notes[G]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt,2*notes[C]);
 
-   
-    freqout(PIN,qnt/2,2*notes[C]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,3*qnt/2,2*notes[D]);
-    freqout(PIN,qnt/4,2*notes[C]);
-    freqout(PIN,qnt/2,2*notes[C]);
-    freqout(PIN,qnt/2,notes[Asharp]);
-    freqout(PIN,qnt,2*notes[Asharp]);
-    freqout(PIN,3*qnt/2,2*notes[Asharp]);  
-    freqout(PIN,qnt/2,notes[G]);
-    freqout(PIN,3*qnt/2,2*notes[C]);
-    freqout(PIN,qnt/4,2*notes[Asharp]);
+freqout(PIN,qnt/2,2*notes[C]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,3*qnt/2,2*notes[D]);
+freqout(PIN,qnt/4,2*notes[C]);
+freqout(PIN,qnt/2,2*notes[C]);
+freqout(PIN,qnt/2,notes[Asharp]);
+freqout(PIN,qnt,2*notes[Asharp]);
+freqout(PIN,3*qnt/2,2*notes[Asharp]);
+freqout(PIN,qnt/2,notes[G]);
+freqout(PIN,3*qnt/2,2*notes[C]);
+freqout(PIN,qnt/4,2*notes[Asharp]);
 
-    freqout(PIN,qnt/2,2*notes[Asharp]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,qnt,2*notes[A]);
-    freqout(PIN,qnt,notes[C]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt,notes[G]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,qnt/2,2*notes[A]);
-    freqout(PIN,3*qnt/2,2*notes[A]);
-    freqout(PIN,qnt/4,2*notes[A]);//MF
-    
-    freqout(PIN,qnt/2,notes[G]);
-    //freqout(PIN,qnt,notes[F]);
-    freqout(PIN,qnt,2*notes[Asharp]);
-    freqout(PIN,qnt,2*notes[E]);
-    freqout(PIN,qnt/2,notes[G]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,2*qnt,notes[F]);
+freqout(PIN,qnt/2,2*notes[Asharp]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,qnt,2*notes[A]);
+freqout(PIN,qnt,notes[C]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt,notes[G]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,qnt/2,2*notes[A]);
+freqout(PIN,3*qnt/2,2*notes[A]);
+freqout(PIN,qnt/4,2*notes[A]);//MF
+freqout(PIN,qnt/2,notes[G]);
+//freqout(PIN,qnt,notes[F]);
+freqout(PIN,qnt,2*notes[Asharp]);
+freqout(PIN,qnt,2*notes[E]);
+freqout(PIN,qnt/2,notes[G]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,2*qnt,notes[F]);
 
-    freqout(PIN,qnt/2,notes[G]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,2*qnt,notes[F]);
-    freqout(PIN,qnt/2,notes[G]);
-    freqout(PIN,qnt/2,notes[F]);
-    freqout(PIN,2*qnt,notes[F]);
+freqout(PIN,qnt/2,notes[G]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,2*qnt,notes[F]);
+freqout(PIN,qnt/2,notes[G]);
+freqout(PIN,qnt/2,notes[F]);
+freqout(PIN,2*qnt,notes[F]);
 
-  }
+}
 }
 
 void Octave(float Abase, int *notes)
 {
-  float n = 0.083333; //  This decimal is 1/12  that is 12 steps in an octave.
-   for (int i = 0; i < 12; i++)
-  {
-    notes[i] = (int)(Abase * pow(2, i * n));
-  }
+float n = 0.083333; // This decimal is 1/12 that is 12 steps in an octave.
+for (int i = 0; i < 12; i++)
+{
+notes[i] = (int)(Abase * pow(2, i * n));
+}
 }
 
-/*void Octave(float Abase, int *notes)  // this accomplishes the same thing without using a for loop. 
+/*void Octave(float Abase, int *notes) // this accomplishes the same thing without using a for loop.
 
 {
-  float n = 0.083333; // set the power function
+float n = 0.083333; // set the power function
 
-  notes[0] = Abase*pow(2,0*n);                //all of the notes A#-G# in the octave above
-  notes[1] = Abase*pow(2,n);
-  notes[2] = Abase*pow(2,2*n);
-  notes[3] = Abase*pow(2,3*n);
-  notes[4] = Abase*pow(2,4*n);
-  notes[PIN] = Abase*pow(2,PIN*n);
-  notes[6] = Abase*pow(2,6*n);
-  notes[7] = Abase*pow(2,7*n);
-  notes[8] = Abase*pow(2,8*n);
-  notes[9] = Abase*pow(2,9*n);
-  notes[10] = Abase*pow(2,10*n);
-  notes[11] = Abase*pow(2,11*n);
+notes[0] = Abase*pow(2,0*n); //all of the notes A#-G# in the octave above
+notes[1] = Abase*pow(2,n);
+notes[2] = Abase*pow(2,2*n);
+notes[3] = Abase*pow(2,3*n);
+notes[4] = Abase*pow(2,4*n);
+notes[PIN] = Abase*pow(2,PIN*n);
+notes[6] = Abase*pow(2,6*n);
+notes[7] = Abase*pow(2,7*n);
+notes[8] = Abase*pow(2,8*n);
+notes[9] = Abase*pow(2,9*n);
+notes[10] = Abase*pow(2,10*n);
+notes[11] = Abase*pow(2,11*n);
 }
 */
